265ac232426a18c27d33a54cdb81e6571afd4343
NestorOtzx <nestorortiz017@gmail.com>
2026-05-22 15:59:29 -0500

    Ignore local Solution workspace

--- Solution/terraform/modules/compute/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "project_name" { type = string }
-variable "environment" { type = string }
-variable "lab_role_arn" { type = string }
-variable "sensor_table_name" { type = string }
--- Solution/terraform/modules/iot/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "edge_gateway_thing_name" {
-  value = aws_iot_thing.edge_gateway.name
-}
